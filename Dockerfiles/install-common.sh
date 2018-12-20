7de4652d4683d93a14e7d82e2279f0395c7ba5f6
vtejatv <tejaveeramacheneni18@mittymonarch.com>
2018-12-20 11:26:27 -0800

    Update install-common.sh

--- Dockerfiles/install-common.sh
+++ Dockerfiles/install-common.sh
@@ -16,13 +16,6 @@ done
 
 ### prepare an unprivileged user
 
-groupadd -r tensorflow_cc
-useradd --no-log-init -r -m -g tensorflow_cc tensorflow_cc
-chmod -R go+rX /tensorflow_cc
-function unpriv-run() {
-    sudo --preserve-env=LD_LIBRARY_PATH -H -u tensorflow_cc "$@"
-}
-
 ### build and install tensorflow_cc ###
 
 mkdir tensorflow_cc/tensorflow_cc/build
@@ -30,12 +23,6 @@ chown -R tensorflow_cc:tensorflow_cc tensorflow_cc/tensorflow_cc/build
 chmod go+rX tensorflow_cc/tensorflow_cc/build
 cd tensorflow_cc/tensorflow_cc/build
 
-# configure only shared or only static library
-if $shared; then
-    cmake -DTENSORFLOW_STATIC=OFF -DTENSORFLOW_SHARED=ON ..;
-else
-    cmake ..;
-fi
 
 # build and install
 make
@@ -43,17 +30,3 @@ rm -rf /home/tensorflow_cc/.cache
 make install
 cd "$cwd"
 rm -rf tensorflow_cc/tensorflow_cc/build
-
-### build and run example ###
-
-mkdir tensorflow_cc/example/build
-chown -R tensorflow_cc:tensorflow_cc tensorflow_cc/example/build
-chmod go+rX tensorflow_cc/example/build
-cd tensorflow_cc/example/build
-cmake ..
-make
-./example
-
-### delete the unprivileged user
-chown -R root:root /tensorflow_cc
-userdel -r tensorflow_cc
